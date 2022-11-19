b843895a61ab1b787e85fb277d89b5b215a9aa55
karam mustafa <karam2mustafa@gmail.com>
2022-11-19 13:48:52 +0200

    first template

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function(){
+    if(this.scrollY > 100){
+    navbar.classList.add('nav-dark')
+}else{
+    navbar.classList.remove('nav-dark')
+}
+});
